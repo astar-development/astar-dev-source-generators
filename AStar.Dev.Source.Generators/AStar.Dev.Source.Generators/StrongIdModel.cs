2756e848e9736eb605b8cd09f62851216c283d2f
Jason Barden <jason.barden1@outlook.com>
2026-01-01 10:56:13 +0000

    Move StrongIdModelEqualityComparer to its own file
    Refactored code by relocating StrongIdModelEqualityComparer from StrongIdModel.cs to StrongIdModelEqualityComparer.cs for better separation of concerns. Also removed unused using directives from StrongIdModel.cs. No changes to comparer implementation.

--- AStar.Dev.Source.Generators/AStar.Dev.Source.Generators/StrongIdModel.cs
+++ AStar.Dev.Source.Generators/AStar.Dev.Source.Generators/StrongIdModel.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 
 namespace AStar.Dev.Source.Generators;
 
@@ -10,17 +8,4 @@ internal sealed class StrongIdModel(string? ns, string name, Accessibility acces
     public string Name { get; } = name;
     public Accessibility Accessibility { get; } = accessibility;
     public string UnderlyingTypeDisplay { get; } = underlyingTypeDisplay;
-}
-
-internal sealed class StrongIdModelEqualityComparer : IEqualityComparer<StrongIdModel>
-{
-    public static readonly StrongIdModelEqualityComparer Instance = new();
-
-    public bool Equals(StrongIdModel? x, StrongIdModel? y)
-        => ReferenceEquals(x, y) || (x is not null && y is not null && string.Equals(x.Namespace, y.Namespace, StringComparison.Ordinal) &&
-               string.Equals(x.Name, y.Name, StringComparison.Ordinal) &&
-               string.Equals(x.UnderlyingTypeDisplay, y.UnderlyingTypeDisplay, StringComparison.Ordinal) &&
-               x.Accessibility == y.Accessibility);
-
-    public int GetHashCode(StrongIdModel obj) => (obj.Namespace, obj.Name, obj.UnderlyingTypeDisplay, obj.Accessibility).GetHashCode();
 }
---
+++ AStar.Dev.Source.Generators/AStar.Dev.Source.Generators/StrongIdModelEqualityComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AStar.Dev.Source.Generators;
+
+internal sealed class StrongIdModelEqualityComparer : IEqualityComparer<StrongIdModel>
+{
+    public static readonly StrongIdModelEqualityComparer Instance = new();
+
+    public bool Equals(StrongIdModel? x, StrongIdModel? y)
+        => ReferenceEquals(x, y) || (x is not null && y is not null && string.Equals(x.Namespace, y.Namespace, StringComparison.Ordinal) &&
+               string.Equals(x.Name, y.Name, StringComparison.Ordinal) &&
+               string.Equals(x.UnderlyingTypeDisplay, y.UnderlyingTypeDisplay, StringComparison.Ordinal) &&
+               x.Accessibility == y.Accessibility);
+
+    public int GetHashCode(StrongIdModel obj) => (obj.Namespace, obj.Name, obj.UnderlyingTypeDisplay, obj.Accessibility).GetHashCode();
+}
